3ea1145b9df449db241ed35dd587cd83c049c854
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-01-30 22:43:05 +0100

    switched to 8bit colors

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 #include <DigiCDC.h>
 
-char chr;
 uint32_t color = 0;
-
-#define IDLE 0
-#define NORMAL 0xff
-#define VISUAL 0xffff00
-#define SELECT 0xff00ff
-#define INSERT 0xffffffff
-#define CMD 0xff0000
-#define EX 0xff1000
-#define TERMINAL 0x00ff00
 
 #define RX 4
 #define TX 3
@@ -20,62 +10,29 @@ Adafruit_NeoPixel strip(1, 1, NEO_GRB + NEO_KHZ800);
 
 extern "C" void setup() {
   strip.begin();
-  strip.show();
-  strip.setBrightness(50);
-
-  SerialUSB.delay(2000);
-  strip.setPixelColor(0, 0xff);
-  strip.show();
+  strip.clear();
+  strip.setBrightness(255);
 
   SerialUSB.begin();
-  SerialUSB.write(F("Serial initialization complete"));
 
   strip.setPixelColor(0, 0xff00);
   strip.show();
-  SerialUSB.delay(500);
-  strip.setPixelColor(0, 0);
+  SerialUSB.delay(1000);
+  strip.clear();
   strip.show();
-  
 }
 
 extern "C" void loop() {
-  
+    if (SerialUSB.available()) {
+        int val = SerialUSB.read();
 
-    if (SerialUSB.available())
-    {
-        chr = SerialUSB.read();
-
-        switch (chr) {
-          case '0':
-            color = IDLE;
-            break;
-          case '1':
-            color = NORMAL;
-            break;
-          case '2':
-            color = VISUAL;
-            break;
-          case '3':
-            color = SELECT;
-            break;
-          case '4':
-            color = INSERT;
-            break;
-          case '5':
-            color = CMD;
-            break;
-          case '6':
-            color = EX;
-            break;
-          case '7':
-            color = TERMINAL;
-            break;
-          default:
-            break;
+        if (val != -1) {
+          SerialUSB.write(color);
+          color = ((val & 0b11000000) << 24) | ((val & 0b110000) << 18)
+            | ((val & 0b1100) << 12) | ((val & 0b11) << 6);
         }
+
         strip.setPixelColor(0, color);
         strip.show();
     }
-    SerialUSB.delay(10);
-
 }
