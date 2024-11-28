42cad1544277904623dabb7e74aba9c00c3d833a
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-11-28 22:36:11 +0100

    improve program name

--- src/rubber-duckies/good.cpp
+++ src/rubber-duckies/good.cpp
@@ -46,11 +46,11 @@ void duckies::good(const long delay) {
   if (!done) {
     DigiKeyboard.delay(5000);
     DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT);
-    DigiKeyboard.delay(200);
-    DigiKeyboard.println("console");
-    DigiKeyboard.delay(2000);
+    DigiKeyboard.delay(500);
+    DigiKeyboard.println("terminal");
+    DigiKeyboard.delay(1000);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(2000);
+    DigiKeyboard.delay(3000);
     unsigned int idx = get_random() % (sizeof(quotes) / sizeof(quotes[0]));
     DigiKeyboard.println((__FlashStringHelper *)pgm_read_word(&quotes[idx]));
     done = true;
